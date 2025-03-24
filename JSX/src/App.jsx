4cef67804e281887134452e48624411153029900
Ritik <ritikroshansingh836@gmail.com>
2025-03-24 20:06:16 +0530

    JSX

---
+++ JSX/src/App.jsx
@@ -0,0 +1,14 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>JSX : JSX (JavaScript XML) is a syntax extension for JavaScript that allows you to write HTML-like code inside JavaScript. It makes writing UI components easier and more readable in React.</h1>
+      <hr />
+      <div>
+        <img src="ritikroshan.jpg" alt="not found" width="200px" height="200px" />
+      </div>
+      <h2>Ritik Roshan</h2>
+      <h3> Hello , how are you friends..!</h3>
+      <p>summary : Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste doloremque minus eligendi voluptas exercitationem voluptates dignissimos saepe amet commodi aut!</p>
+    </div>
+  );
+}
