cb0bb93e750f5296b6c162bac4a130d18049e446
Ritik <ritikroshansingh836@gmail.com>
2025-03-27 13:25:24 +0530

    CSS modules

---
+++ EP-15 CSS modules/src/components/SeriesList.jsx
@@ -0,0 +1,40 @@
+import styles from "./NetFlix.module.css";
+
+export const SeriesCard = (props) => {
+    const { img_url, name, rating, description, genre, cast, watch_url } = props.data;
+
+
+    const btn_style = {
+        padding: "1.2rem 2.4rem",
+        border: "none",
+        fontSize: "1.6rem",
+        backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+        color: "var(--btn-color)",
+        fontWeight: "bold",
+        cursor: "pointer"
+    }
+    
+    const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average;
+
+    return (
+        <li className={styles.card}>
+            <div>
+                <img src={img_url} alt="not found" width="400px" height="200px" />
+            </div>
+            <div className={styles['card-content']}>
+                <h2>Name :  {name}</h2>
+
+                <h3>Rating :<span className={`${styles.rating} ${ratingClass}`}> {rating} </span></h3>
+
+                <p>summary :  {description} </p>
+
+                <p>Genre :  {genre} </p>
+
+                <p>Cast :  {cast} </p>
+
+                <a href={watch_url} target="_blank">
+                    <button style={btn_style}>Watch Now</button>
+                </a>
+            </div>
+        </li>)
+};
